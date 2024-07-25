dc61803200e666a29ed2c830c7481500f2da9816
Cuong <cuongnv.goat@gmail.com>
2024-07-25 15:53:25 +0700

    code app bar

---
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+import Box from '@mui/material/Box'
+
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Divider from '@mui/material/Divider'
+
+import ListItemIcon from '@mui/material/ListItemIcon'
+
+import Tooltip from '@mui/material/Tooltip'
+import IconButton from '@mui/material/IconButton'
+import Avatar from '@mui/material/Avatar'
+import PersonAdd from '@mui/icons-material/PersonAdd'
+import Settings from '@mui/icons-material/Settings'
+import Logout from '@mui/icons-material/Logout'
+function Profiles() {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <Box>
+      <Tooltip title='Account settings'>
+        <IconButton
+          onClick={handleClick}
+          size='small'
+          sx={{ padding: 0 }}
+          aria-controls={open ? 'basic-menu-profiles' : undefined}
+          aria-haspopup='true'
+          aria-expanded={open ? 'true' : undefined}
+        >
+          <Avatar
+            sx={{ width: 32, height: 32 }}
+            alt='gojo-satoru'
+            src='https://i.pinimg.com/564x/0c/25/42/0c25424c87b5184c185d84c2d40a99de.jpg'
+          />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        id='basic-menu-profiles'
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button-profiles',
+        }}
+      >
+        <MenuItem>
+          <Avatar sx={{ mr: 2, width: '28px', height: '28px' }} /> Profile
+        </MenuItem>
+        <MenuItem>
+          <Avatar sx={{ mr: 2, width: '28px', height: '28px' }} /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <PersonAdd fontSize='small' />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize='small' />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Logout fontSize='small' />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default Profiles
